4ec4516d20398207970f12abbaa8a60db3facdb3
sadan <117494111+sadan4@users.noreply.github.com>
2024-09-09 02:09:05 -0400

    add plugin

--- index.tsx
+++ index.tsx
@@ -4,15 +4,43 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+let observer: MutationObserver | undefined = undefined;
+
+const settings = definePluginSettings({
+    title: {
+        type: OptionType.STRING,
+        description: "The static window title",
+        default: "Discord"
+    }
+});
 
 export default definePlugin({
     name: "StaticTitle",
-    description: "",
-    authors: [
-        {
-            name: "sadan",
-            id: 521819891141967883n
-        }
-    ]
+    hidden: IS_VESKTOP,
+    description: "Gives a static window title",
+    authors: [Devs.sadan],
+    settings,
+    start() {
+        if(IS_VESKTOP) return;
+
+        const title = document.querySelector("title");
+
+        if(!title) throw new Error("could not find title");
+
+        observer = new MutationObserver(() => {
+            if(title.innerHTML === settings.store.title) return;
+            title.innerHTML = settings.store.title;
+        });
+
+        observer.observe(title, {
+            childList: true
+        });
+    },
+    stop(){
+        observer?.disconnect();
+    }
 });

6f7521d2c4f739448940bbd9542115c41cd3fa94
sadan <117494111+sadan4@users.noreply.github.com>
2024-09-09 02:08:18 -0400

    create plugin StaticTitle

---
+++ index.tsx
@@ -0,0 +1,18 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "StaticTitle",
+    description: "",
+    authors: [
+        {
+            name: "sadan",
+            id: 521819891141967883n
+        }
+    ]
+});
